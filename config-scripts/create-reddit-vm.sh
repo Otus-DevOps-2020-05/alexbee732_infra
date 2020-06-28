9ab092ed0c07a3fba5e1a36909f4c770d89067d5
Alexey Babenko <babenko732@gmail.com>
2020-06-28 21:40:28 +0300

    packer homework

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -3,7 +3,7 @@ yc compute instance create \
 --name reddit-app \
 --hostname reddit-app \
 --memory=4 \
---create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
+--create-boot-disk name=disk1,size=10,image-id=fd80h8459h9o24m97m9k \
 --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
 --metadata serial-port-enable=1 \
 --metadata-from-file user-data=reddit_instance_config.yaml
