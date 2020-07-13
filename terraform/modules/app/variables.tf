0134c07f25e29c6d5d429c066280f0d5a9ef9d5e
Alexey Babenko <babenko732@gmail.com>
2020-07-13 11:28:11 +0300

    hw 9

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,10 @@
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable subnet_id {
+  description = "subnet id"
+}
+variable public_key_path {
+  description = "public key path"
+}
